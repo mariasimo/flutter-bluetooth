2a786eaf5f12c8ee6176cc723c3f03ffad9e342e
María Simó <maria.simo@secuoyas.com>
2021-04-21 23:50:59 +0200

    add connect button to card, improve layout

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,13 @@ class BluetoothApp extends StatefulWidget {
 class _BluetoothAppState extends State<BluetoothApp> {
   BluetoothState bluetoothState = BluetoothState.UNKNOWN;
   FlutterBluetoothSerial bluetoothInstance = FlutterBluetoothSerial.instance;
-  List<BluetoothDevice> devicesList = []; // var for storing the devices list
   BluetoothConnection connection; // Track connection with the remote device
   bool get isConnected =>
       connection != null && connection.isConnected; // still connected to BT
   bool isDisconnecting = false; // disconnection is in progress
+  List<BluetoothDevice> devicesList = []; // var for storing the devices list
+  int _deviceState;
+  BluetoothDevice _device;
 
   @override
   void dispose() {
@@ -40,7 +42,6 @@ class _BluetoothAppState extends State<BluetoothApp> {
       print("Error");
     }
     if (!mounted) return;
-    print({devicesList});
     setState(() => devicesList = devices);
   }
 
@@ -52,19 +53,24 @@ class _BluetoothAppState extends State<BluetoothApp> {
     // and then retrieve the devices that are paired.
     if (bluetoothState == BluetoothState.STATE_OFF) {
       await FlutterBluetoothSerial.instance.requestEnable();
+      return true;
     }
     await getPairedDevices();
+    return false;
+  }
+
+  void _connect() async {
+    if (_device == null) {
+      // show('No device selected');
+    } else {
+      // If a device is selected from the
+      // dropdown, then use it here
+    }
   }
 
   @override
   void initState() {
     super.initState();
-    FlutterBluetoothSerial.instance.state.then((state) {
-      print({"initState", state});
-      setState(() {
-        bluetoothState = state;
-      });
-    });
 
     enableBluetooth();
 
@@ -87,14 +93,14 @@ class _BluetoothAppState extends State<BluetoothApp> {
         title: Text('Testing Bluetooth'),
       ),
       body: Container(
+        padding: EdgeInsets.all(16),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
           children: <Widget>[
             SwitchListTile(
+              contentPadding: EdgeInsets.all(0),
               title: const Text('Enable Bluetooth'),
               value: bluetoothState.isEnabled,
               onChanged: (bool value) {
-                print({'isBTEnabled', value});
                 // Do the request and update with the true value then
                 future() async {
                   // async lambda seems to not working
@@ -105,21 +111,56 @@ class _BluetoothAppState extends State<BluetoothApp> {
                 }
 
                 future().then((_) {
-                  print({'isBTEnabled', value});
                   setState(() {});
                 });
               },
             ),
-            ListView.builder(
-                physics: NeverScrollableScrollPhysics(),
-                shrinkWrap: true,
-                itemCount: devicesList.length,
-                itemBuilder: (context, index) {
-                  return Card(
-                    child: ListTile(
-                        onTap: () {}, title: Text(devicesList[index].name)),
-                  );
-                })
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Paired devices",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+                SizedBox(height: 4),
+                ListView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  itemCount: devicesList.length,
+                  itemBuilder: (context, index) {
+                    return Card(
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(
+                            vertical: 0, horizontal: 8),
+                        child: Column(
+                          children: [
+                            ListTile(
+                              onTap: () {},
+                              title: Text(devicesList[index].name),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: <Widget>[
+                                TextButton(
+                                  child: const Text('CONNECT'),
+                                  onPressed: () {
+                                    setState(
+                                        () => _device = devicesList[index]);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                )
+              ],
+            ),
           ],
         ),
       ),
